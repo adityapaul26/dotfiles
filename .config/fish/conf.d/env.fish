16e8438ffcbcd300ef61d746e6da23cf83bb4a61
adityapaul26 <adityapaul2603@gmail.com>
2026-09-04 20:40:18 +0530

    floci variables add

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -5,6 +5,12 @@
 # Set default editor to Neovim
 set -gx EDITOR nvim
 
+# Set AWS environment variables for floci. these are obviously fake
+set -x AWS_ENDPOINT_URL http://localhost:4566
+set -x AWS_DEFAULT_REGION us-east-1
+set -x AWS_ACCESS_KEY_ID test
+set -x AWS_SECRET_ACCESS_KEY test
+
 # Add local and npm-global binaries to PATH
 fish_add_path ~/.local/bin
 fish_add_path ~/.npm-global/bin
